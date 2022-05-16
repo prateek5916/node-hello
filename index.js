eb44ba19c94cffd7b1ddfa282945c222b4f07039
prateek5916 <prateek.khandelwal@tothenew.com>
2022-05-16 09:27:33 +0530

    Update JS

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'MY NAME IS  KHAN AND I AM NOT A TERRIOST!\n'
+  const msg = 'MY  IS  KHAN AND I AM NOT A TERRIOST!\n'
   res.end(msg);
 });
 
